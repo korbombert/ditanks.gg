506b519442860d12c359db599bc6b3a66229467d
korbombert <christian_resmerita@yahoo.com>
2026-05-08 18:53:15 +0100

    Update client.js

--- public/client.js
+++ public/client.js
@@ -659,7 +659,7 @@ function checkUpgrades() {
     if (myStats.level >= 15 && myStats.tankType === 'Basic') {
         options = ['Twin', 'Sniper', 'Machine Gun', 'Flank Guard']; 
     } else if (myStats.level >= 30) {
-        if (myStats.tankType === 'Sniper') options = ['Overseer'];
+        if (myStats.tankType === 'Sniper') options = ['Overlord'];
         else if (myStats.tankType === 'Machine Gun') options = ['Destroyer'];
         else if (myStats.tankType === 'Twin') options = ['Octo Tank', 'Triplet'];
         else if (myStats.tankType === 'Flank Guard') options = ['Tri-angle', 'Octo Tank'];
